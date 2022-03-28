43d3fa41173e8ad1962a269da081bff43ede1610
Glumboi <58010838+Glumboi@users.noreply.github.com>
2022-03-28 17:02:28 +0200

    Add files via upload

---
+++ EZNotepad_redesign/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace EZNotepad_redesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.edit = new MaterialSkin.Controls.MaterialMultiLineTextBox();
+            this.Slider_Font = new MaterialSkin.Controls.MaterialSlider();
+            this.Update = new System.Windows.Forms.Timer(this.components);
+            this.Cb_Darkmode = new MaterialSkin.Controls.MaterialCheckbox();
+            this.Charcount = new MaterialSkin.Controls.MaterialLabel();
+            this.UpdatePerformCounter = new System.Windows.Forms.Timer(this.components);
+            this.Bn_Open = new MaterialSkin.Controls.MaterialFloatingActionButton();
+            this.Bn_Save = new MaterialSkin.Controls.MaterialFloatingActionButton();
+            this.BN_New = new MaterialSkin.Controls.MaterialFloatingActionButton();
+            this.SuspendLayout();
+            // 
+            // edit
+            // 
+            this.edit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.edit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.edit.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.edit.Depth = 0;
+            this.edit.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.edit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.edit.Location = new System.Drawing.Point(2, 113);
+            this.edit.MouseState = MaterialSkin.MouseState.HOVER;
+            this.edit.Name = "edit";
+            this.edit.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.edit.Size = new System.Drawing.Size(1121, 450);
+            this.edit.TabIndex = 0;
+            this.edit.Text = "";
+            this.edit.DragDrop += new System.Windows.Forms.DragEventHandler(this.edit_dragdrop);
+            this.edit.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.edit_LinkClicked);
+            // 
+            // Slider_Font
+            // 
+            this.Slider_Font.Depth = 0;
+            this.Slider_Font.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Slider_Font.Location = new System.Drawing.Point(6, 67);
+            this.Slider_Font.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Slider_Font.Name = "Slider_Font";
+            this.Slider_Font.RangeMax = 150;
+            this.Slider_Font.RangeMin = 1;
+            this.Slider_Font.Size = new System.Drawing.Size(259, 40);
+            this.Slider_Font.TabIndex = 2;
+            this.Slider_Font.Text = "Font size";
+            this.Slider_Font.Value = 11;
+            // 
+            // Update
+            // 
+            this.Update.Enabled = true;
+            this.Update.Interval = 50;
+            this.Update.Tick += new System.EventHandler(this.Update_Tick);
+            // 
+            // Cb_Darkmode
+            // 
+            this.Cb_Darkmode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Cb_Darkmode.Depth = 0;
+            this.Cb_Darkmode.Location = new System.Drawing.Point(1009, 570);
+            this.Cb_Darkmode.Margin = new System.Windows.Forms.Padding(0);
+            this.Cb_Darkmode.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.Cb_Darkmode.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Cb_Darkmode.Name = "Cb_Darkmode";
+            this.Cb_Darkmode.ReadOnly = false;
+            this.Cb_Darkmode.Ripple = true;
+            this.Cb_Darkmode.Size = new System.Drawing.Size(113, 37);
+            this.Cb_Darkmode.TabIndex = 5;
+            this.Cb_Darkmode.Text = "Darkmode";
+            this.Cb_Darkmode.UseVisualStyleBackColor = true;
+            this.Cb_Darkmode.CheckedChanged += new System.EventHandler(this.Cb_Darkmode_CheckedChanged);
+            // 
+            // Charcount
+            // 
+            this.Charcount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Charcount.AutoSize = true;
+            this.Charcount.Depth = 0;
+            this.Charcount.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.Charcount.Location = new System.Drawing.Point(6, 579);
+            this.Charcount.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Charcount.Name = "Charcount";
+            this.Charcount.Size = new System.Drawing.Size(210, 19);
+            this.Charcount.TabIndex = 6;
+            this.Charcount.Text = "Current document char count:";
+            // 
+            // Bn_Open
+            // 
+            this.Bn_Open.Depth = 0;
+            this.Bn_Open.Icon = global::EZNotepad_redesign.Properties.Resources.Icon_Open;
+            this.Bn_Open.Location = new System.Drawing.Point(286, 67);
+            this.Bn_Open.Mini = true;
+            this.Bn_Open.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Bn_Open.Name = "Bn_Open";
+            this.Bn_Open.Size = new System.Drawing.Size(40, 47);
+            this.Bn_Open.TabIndex = 8;
+            this.Bn_Open.Text = "materialFloatingActionButton1";
+            this.Bn_Open.UseVisualStyleBackColor = true;
+            this.Bn_Open.Click += new System.EventHandler(this.Bn_Open_Click);
+            // 
+            // Bn_Save
+            // 
+            this.Bn_Save.Depth = 0;
+            this.Bn_Save.Icon = global::EZNotepad_redesign.Properties.Resources.Icon_Save2;
+            this.Bn_Save.Location = new System.Drawing.Point(332, 67);
+            this.Bn_Save.Mini = true;
+            this.Bn_Save.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Bn_Save.Name = "Bn_Save";
+            this.Bn_Save.Size = new System.Drawing.Size(40, 47);
+            this.Bn_Save.TabIndex = 9;
+            this.Bn_Save.Text = "materialFloatingActionButton2";
+            this.Bn_Save.UseVisualStyleBackColor = true;
+            this.Bn_Save.Click += new System.EventHandler(this.Bn_Save_Click);
+            // 
+            // BN_New
+            // 
+            this.BN_New.Depth = 0;
+            this.BN_New.Icon = ((System.Drawing.Image)(resources.GetObject("BN_New.Icon")));
+            this.BN_New.Location = new System.Drawing.Point(378, 67);
+            this.BN_New.Mini = true;
+            this.BN_New.MouseState = MaterialSkin.MouseState.HOVER;
+            this.BN_New.Name = "BN_New";
+            this.BN_New.Size = new System.Drawing.Size(40, 47);
+            this.BN_New.TabIndex = 10;
+            this.BN_New.Text = "materialFloatingActionButton3";
+            this.BN_New.UseVisualStyleBackColor = true;
+            this.BN_New.Click += new System.EventHandler(this.BN_New_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 610);
+            this.Controls.Add(this.BN_New);
+            this.Controls.Add(this.Bn_Save);
+            this.Controls.Add(this.Bn_Open);
+            this.Controls.Add(this.Charcount);
+            this.Controls.Add(this.Cb_Darkmode);
+            this.Controls.Add(this.Slider_Font);
+            this.Controls.Add(this.edit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EZNotepad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialMultiLineTextBox edit;
+        private MaterialSkin.Controls.MaterialSlider Slider_Font;
+        private System.Windows.Forms.Timer Update;
+        private MaterialSkin.Controls.MaterialCheckbox Cb_Darkmode;
+        private MaterialSkin.Controls.MaterialLabel Charcount;
+        private System.Windows.Forms.Timer UpdatePerformCounter;
+        private MaterialSkin.Controls.MaterialFloatingActionButton Bn_Open;
+        private MaterialSkin.Controls.MaterialFloatingActionButton Bn_Save;
+        private MaterialSkin.Controls.MaterialFloatingActionButton BN_New;
+    }
+}
+
---
+++ EZNotepad_redesign/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using Microsoft.Win32;
+
+namespace EZNotepad_redesign
+{
+    public partial class Form1 : MaterialForm
+    {
+        //Variables used front-end
+        string text;
+        bool darkmode;
+        int fontsize_;
+        MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+
+        //Extern methods used to manage hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        //Hotkeys
+        private const int ACTION_HOTKEY_ID = 1;
+        private const int ACTION_HOTKEY_ID2 = 2;
+        private const int ACTION_HOTKEY_ID3 = 3;
+        private const int ACTION_HOTKEY_ID4 = 4;
+        private const int ACTION_HOTKEY_ID5 = 5;
+        private const int ACTION_HOTKEY_ID6 = 6;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Orange700, Primary.Orange700, Primary.Orange700, Accent.Orange700, TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadContent_Settings();
+            }
+            catch
+            {
+                Slider_Font.Value = 11;
+            }
+        }
+
+        private void LoadContent_Settings()
+        {
+            //This registers the hotkey presses
+            RegisterHotKey(this.Handle, ACTION_HOTKEY_ID, 2, (int)Keys.S);
+            RegisterHotKey(this.Handle, ACTION_HOTKEY_ID2, 2, (int)Keys.O);
+            RegisterHotKey(this.Handle, ACTION_HOTKEY_ID3, 2, (int)Keys.N);
+            RegisterHotKey(this.Handle, ACTION_HOTKEY_ID4, 2, (int)Keys.G);
+            RegisterHotKey(this.Handle, ACTION_HOTKEY_ID5, 2, (int)Keys.K);
+            RegisterHotKey(this.Handle, ACTION_HOTKEY_ID6, 2, (int)Keys.E);
+
+            getsettings();
+        }
+
+        //This method handles all the hotkey actions
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == ACTION_HOTKEY_ID)
+            {
+                savefile();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == ACTION_HOTKEY_ID2)
+            {
+                openfile();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == ACTION_HOTKEY_ID3)
+            {
+                newfile();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == ACTION_HOTKEY_ID4)
+            {
+                Process.Start("https://github.com/Glumboi");
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == ACTION_HOTKEY_ID5)
+            {
+                Process.Start("https://google.com");
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == ACTION_HOTKEY_ID6)
+            {
+                if (string.IsNullOrWhiteSpace(edit.Text))
+                {
+                    edit.Text = addtocurrenttext($"EZTextEditor version: {getversionofapp()}");
+                }
+                else
+                {
+                    edit.Text = addtocurrenttext($" EZTextEditor version: {getversionofapp()}");
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private string addtocurrenttext(string stringtoadd)
+        {
+            string text_ = edit.Text + stringtoadd;
+            return text_;
+        }
+
+        private string getversionofapp()
+        {
+            var os = RuntimeInformation.OSDescription;
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString() + "\nOS version: " + os;
+        }
+
+        private void newfile()
+        {
+            edit.Clear();
+        }
+
+        private void Update_Tick(object sender, EventArgs e)
+        {
+            loadfontsize_textlength();
+            int fontsize = Slider_Font.Value;
+            try
+            {
+                setfont(fontsize);
+            }
+            catch
+            {
+                fontsize = 1;
+            }
+        }
+
+        private void loadfontsize_textlength()
+        {
+            string textlength = edit.Text.Length.ToString();
+            Charcount.Text = $"Current document char count:  {textlength}";
+        }
+
+        private void setfont(int size)
+        {
+            //Properties.Settings.Default.FontSize = size;
+            edit.Font = new Font(edit.Font.FontFamily, size, edit.Font.Style);
+        }
+
+        private void savefile()
+        {
+            text = edit.Text;
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = ".txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            save.FilterIndex = 1;
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                //---Old code kept for reference---//
+                //File.WriteAllText(save.FileName, text);
+
+                string savelocation = save.FileName;
+                edit.SaveFile(savelocation, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        //Creates a openfiedialog which will open the file on the selected path
+        private void openfile()
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = ".txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                edit.LoadFile(file.FileName, RichTextBoxStreamType.PlainText);
+            }
+
+            //The following two lines are used to update the GUI
+            //--------------------------------------------------
+            //This is old code kept for reference or if i revert
+            //string output = File.ReadAllText(file.FileName);
+            //edit.Text = output;
+            //--------------------------------------------------   
+        }
+
+        private void Bn_Save_Click(object sender, EventArgs e)
+        {
+            savefile();
+        }
+
+        private void Bn_Open_Click(object sender, EventArgs e)
+        {
+            openfile();
+        }
+
+        private void Cb_Darkmode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Cb_Darkmode.Checked)
+            {
+                darkmode = true;
+                changetodark();
+            }
+            else if (darkmode)
+            {
+                darkmode = false;
+                changetowhite();
+            }
+        }
+
+        private void changetodark()
+        {
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Orange700, Primary.Orange700, Primary.Orange700, Accent.Orange700, TextShade.WHITE);
+        }
+
+        private void changetowhite()
+        {
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Orange700, Primary.Orange700, Primary.Orange700, Accent.Orange700, TextShade.WHITE);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            savesettings();
+        }
+
+        private void getsettings()
+        {
+            darkmode = Properties.Settings.Default.Darkmode;
+            fontsize_ = Properties.Settings.Default.FontSize;
+            Slider_Font.Value = fontsize_;
+            setfont(fontsize_);
+            if (darkmode)
+            {
+                Cb_Darkmode.Checked = true;
+            }
+            else
+            {
+                Cb_Darkmode.Checked = false;
+            }
+        }
+
+        private void savesettings()
+        {
+            Properties.Settings.Default.Darkmode = darkmode;
+            fontsize_ = getfontsize();
+            Properties.Settings.Default.FontSize = fontsize_;
+            Properties.Settings.Default.Save();
+        }
+
+        private int getfontsize()
+        {
+            return (int)edit.Font.Size;
+        }
+
+        private void BN_New_Click(object sender, EventArgs e)
+        {
+            edit.Clear();
+        }
+
+        //Method to load dragged files
+        private void edit_dragdrop(object sender, DragEventArgs e)
+        {
+            object filename = e.Data.GetData("FileDrop");
+            if (filename != null)
+            {
+                var list = filename as string[];
+
+                if (list != null && !string.IsNullOrWhiteSpace(list[0]))
+                {
+                    edit.Clear();
+                    edit.LoadFile(list[0], RichTextBoxStreamType.PlainText);
+                }
+            }
+        }
+
+        private void edit_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            Process.Start(e.LinkText);
+        }
+    }
+}
